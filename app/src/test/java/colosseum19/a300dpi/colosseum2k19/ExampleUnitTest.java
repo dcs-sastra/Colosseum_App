22aa680ddd4faec17300a0aa0879582296eb0c1d
KavinRaju <rajukavin25@gmail.com>
2019-01-28 20:17:14 +0530

    Initial commit

---
+++ app/src/test/java/colosseum19/a300dpi/colosseum2k19/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package colosseum19.a300dpi.colosseum2k19;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
