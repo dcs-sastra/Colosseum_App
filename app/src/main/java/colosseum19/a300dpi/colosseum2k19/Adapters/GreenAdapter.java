742328f5cbad2bde15ecdb3f64f634c4fb8b0760
KavinRaju <rajukavin25@gmail.com>
2019-02-07 23:38:03 +0530

    Fixed UI issue

--- app/src/main/java/colosseum19/a300dpi/colosseum2k19/Adapters/GreenAdapter.java
+++ app/src/main/java/colosseum19/a300dpi/colosseum2k19/Adapters/GreenAdapter.java
@@ -117,7 +117,6 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
                     case 0:
                         loadImage(R.drawable.badminton_men_women,img);
                         loadImage(R.drawable.icon_badminton, icon_img);
-                        //img.setImageDrawable(context.getResources().getDrawable(R.drawable.badminton_men_women));
                         break;
                     case 1:
                         loadImage(R.drawable.basketball_men_and_women,img);
