cd24d91b6488c401fba81881b7bc1fbcc8ce8419
skr <kavinraju98@gmail.com>
2020-02-04 14:36:53 +0530

    Added Social Medias in About Us

--- app/src/main/java/colosseum19/a300dpi/colosseum2k19/Fragments/AboutFragment.java
+++ app/src/main/java/colosseum19/a300dpi/colosseum2k19/Fragments/AboutFragment.java
@@ -1,6 +1,8 @@
 package colosseum19.a300dpi.colosseum2k19.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +13,9 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import colosseum19.a300dpi.colosseum2k19.R;
+import colosseum19.a300dpi.colosseum2k19.Utilities.ConstantsStorage;
 
 public class AboutFragment extends Fragment {
 
@@ -24,8 +28,8 @@ public class AboutFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.frag_about,container,false);
-        ButterKnife.bind(this,view);
+        View view = inflater.inflate(R.layout.frag_about, container, false);
+        ButterKnife.bind(this, view);
         context = view.getContext();
         dscLogo = view.findViewById(R.id.dsc_logo);
         dscLogo.setImageResource(R.drawable.dsc_logo);
@@ -42,4 +46,43 @@ public class AboutFragment extends Fragment {
         super.onSaveInstanceState(outState);
     }
 
+    @OnClick(R.id.dsc_logo)
+    public void onClickDSCLogo(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String url = ConstantsStorage.DSC_APP_URL;
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.dpi_logo)
+    public void onClickdpiLogo(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String url = ConstantsStorage.DPI_LINKEDIN_URL;
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.dsc_insta)
+    public void onClickdsc_insta(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String url = ConstantsStorage.DSC_INSTA_URL;
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.dsc_linkedin)
+    public void onClickdsc_linkedin(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String url = ConstantsStorage.DSC_LINKEDIN_URL;
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.dsc_twitter)
+    public void onClickdsc_twitter(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String url = ConstantsStorage.DSC_TWITTER_URL;
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+    }
 }
